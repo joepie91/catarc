91365814b865b63ae2d95cf3df2eacd410685101
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-10-11 00:33:14 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script. See inline comments for packaging documentation.
+"""
+
+import os
+import sys
+
+import seesaw
+
+try:
+  from setuptools import setup
+  # hush pyflakes
+  setup
+except ImportError:
+  from distutils.core import setup
+
+try:
+  from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+  from distutils.command.build_py import build_py
+
+packages = []
+
+package_dir = {}
+
+package_data = {}
+
+scripts = [
+  'catarc'
+]
+
+
+setup(
+  name='catarc',
+  version='1.0',
+  maintainer='Sven Slootweg',
+  maintainer_email='admin@cryto.net',
+  description='Tool to output uncompressed contents of (multiple) archives to stdout, without writing to disk',
+  url='http://www.cryto.net/projects/catarc/',
+  packages=packages,
+  package_dir=package_dir,
+  package_data=package_data,
+  include_package_data=True,
+  scripts=scripts,
+  install_requires=[],
+  cmdclass={'build_py': build_py}
+)
+
